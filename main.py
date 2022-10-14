0a50fa3111d12c1dd42abadbeb05e8b6d038b3d8
rousseauolivier <35579427+rousseauolivier@users.noreply.github.com>
2022-10-13 21:58:22 -0400

    test ?

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import os
 import discord
 from discord.ext import commands
 
+TOKEN = os.environ['DISCORD_TOKEN']
+GUILD = os.environ['DISCORD_GUILD']
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -14,9 +16,11 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 async def on_ready():
     print(f"Logged in as {bot.user}")
 
+
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
+
 
 @bot.command()
 async def hello(ctx):
